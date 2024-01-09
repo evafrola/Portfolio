ed4034821bdd6ba6e0b6abd0d1abbbcfdc6fd784
Eva <eva_elizabeth_frola@hotmail.com>
2024-01-09 17:59:46 -0300

    Adaptando animacion

--- src/js/init.js
+++ src/js/init.js
@@ -1,6 +1,12 @@
 const dropdownMenu = document.getElementById('dropdown-menu');
 const dropdownButton = document.getElementById('dropdown-button');
 
+document.addEventListener('change', ()=> {
+    if(dropdownButton.classList.contains('hidden') && !dropdownMenu.classList.contains('hidden')){
+        dropdownMenu.classList.remove('hidden')
+    }
+})
+
 dropdownButton.addEventListener('click', () => {
     if(dropdownMenu.classList.contains('hidden')){
         dropdownMenu.classList.remove('hidden')
