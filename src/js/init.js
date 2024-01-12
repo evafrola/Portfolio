95d4f778f930db8fc8531b7f0a4f41cc56618bf0
Eva <eva_elizabeth_frola@hotmail.com>
2024-01-12 17:52:41 -0300

    Nuevo estilo en nav y arreglo de error en nav para moviles

--- src/js/init.js
+++ src/js/init.js
@@ -1,16 +1,18 @@
 const dropdownMenu = document.getElementById('dropdown-menu');
 const dropdownButton = document.getElementById('dropdown-button');
 
-document.addEventListener('change', ()=> {
-    if(dropdownButton.classList.contains('hidden') && !dropdownMenu.classList.contains('hidden')){
-        dropdownMenu.classList.remove('hidden')
+window.addEventListener('resize', ()=> {
+    if(dropdownButton.classList.contains('min-[769px]:hidden') && !dropdownMenu.classList.contains('hidden')){
+        dropdownMenu.classList.add('hidden');
     }
 })
 
 dropdownButton.addEventListener('click', () => {
     if(dropdownMenu.classList.contains('hidden')){
-        dropdownMenu.classList.remove('hidden')
+        dropdownMenu.classList.remove('hidden');
+        dropdownMenu.classList.add('animate-fade-down', 'animate-once', 'animate-ease-in', 'animate-normal', 'animate-fill-forwards')
     } else {
-        dropdownMenu.classList.add('hidden')
+        dropdownMenu.classList.add('hidden');
+        dropdownButton.classList.remove('animate-fade-down', 'animate-once', 'animate-ease-in', 'animate-normal', 'animate-fill-forwards');
     } 
 })
